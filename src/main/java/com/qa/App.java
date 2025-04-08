9cdbdbf94ff15ca0c56eb4482adc7f3aa0330698
Ben <ben@qa.com>
2025-04-08 11:57:04 +0100

    Added Names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("AJ"));
+        System.out.println(sayHelloToSomeone("Sinbad"));
+        System.out.println(sayHelloToSomeone("Rusty"));
+        System.out.println(sayHelloToSomeone("Shadow"));
         System.out.println(sayGoodbye());
     }
 
